cf23c5fc1d20e6d0a2839940f6c3fc161f115eb7
João Gabriel <jgomachado@hotmail.com>
2024-01-25 15:27:37 -0300

    initial commit

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,51 @@
+import { Helmet } from 'react-helmet-async'
+
+import { Pagination } from '@/components/pagination'
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+import { OrdertableFilters } from './order-table-filters'
+import { OrdertableRow } from './order-table-row'
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+
+        <div className="space-y-2.5">
+          <OrdertableFilters />
+
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 8 }).map((_, i) => {
+                  return <OrdertableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} perPage={10} totalCount={105} />
+        </div>
+      </div>
+    </>
+  )
+}
