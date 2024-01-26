f55aada5865b661f926c442af69f920acb4126df
João Gabriel <jgomachado@hotmail.com>
2024-01-26 11:01:38 -0300

    add components

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,18 @@
+import { Helmet } from 'react-helmet-async'
+
+import { MonthRevenueCard } from './month-revenue-card'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+        </div>
+      </div>
+    </>
+  )
+}
