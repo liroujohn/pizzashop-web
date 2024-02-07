5615b1a0360df0873ec5fe1b0888bb87ea697720
João Gabriel <jgomachado@hotmail.com>
2024-02-07 14:45:13 -0300

    add Skeleton

---
+++ src/api/register-restaurant.ts
@@ -0,0 +1,17 @@
+import { api } from '@/lib/axios'
+
+export interface RegisterRestaurantBody {
+  restaurantName: string
+  menagerName: string
+  email: string
+  phone: string
+}
+
+export async function registerRestaurant({
+  restaurantName,
+  menagerName,
+  email,
+  phone,
+}: RegisterRestaurantBody) {
+  await api.post('/restaurants', { email, phone, restaurantName, menagerName })
+}
