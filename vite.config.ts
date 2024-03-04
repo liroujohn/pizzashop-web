0a14c1fae9a81945d4eed5ccb6a6222bf5145e36
João Gabriel <jgomachado@hotmail.com>
2024-03-04 14:40:52 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: '/sign-in',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
